2054436ea9f502d877526a208276cf667e021b62
denizOgut <46030809+denizOgut@users.noreply.github.com>
2021-09-14 10:47:14 +0300

    Add files via upload

---
+++ SayılarıBüyüktenKüçüğeSıralayanProgram.java
@@ -0,0 +1,61 @@
+package sayılarıbüyüktenküçüğesıralayanprogram;
+
+import java.util.Scanner;
+
+public class SayılarıBüyüktenKüçüğeSıralayanProgram {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, c;
+        System.out.print("Birinci sayıyı giriniz: ");
+        a = sc.nextInt();
+
+        System.out.print("İkinci sayıyı giriniz: ");
+        b = sc.nextInt();
+
+        System.out.print("Üçüncü sayıyı giriniz: ");
+        c = sc.nextInt();
+
+        if ((a > b) && (a > c)) {
+            if (b > c) {
+                System.out.println("a > b > c");
+            } else {
+                System.out.println("a > c > b");
+            }
+        } else if ((b > a) && (b > c)) {
+            if (a > c) {
+                System.out.println("b > a > c");
+            } else {
+                System.out.println("b > c > a");
+            }
+
+        } else if ((c > a) && (c > b)) {
+            if (a > b) {
+                System.out.println("c > a > b");
+            } else {
+                System.out.println("c > b > a");
+            }
+        }
+        
+        if ((a < b) && (a < c)) {
+            if (b < c) {
+                System.out.println("a < b < c");
+            } else {
+                System.out.println("a < c < b");
+            }
+        } else if ((b < a) && (b < c)) {
+            if (a < c) {
+                System.out.println("b < a < c");
+            } else {
+                System.out.println("b < c < a");
+            }
+
+        } else if ((c < a) && (c < b)) {
+            if (a < b) {
+                System.out.println("c < a < b");
+            } else {
+                System.out.println("c < b < a");
+            }
+        }
+    }
+}
