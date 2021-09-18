c7ceae9042817b1ada50e897441853f5e847049e
denizOgut <46030809+denizOgut@users.noreply.github.com>
2021-09-18 17:47:59 +0300

    Add files via upload

---
+++ YıldızileÜçgenYapımı.java
@@ -0,0 +1,32 @@
+package yıldızileüçgenyapımı;
+
+import java.util.Scanner;
+
+public class YıldızileÜçgenYapımı {
+
+    public static void main(String[] args) {
+        Scanner klavye = new Scanner(System.in);
+        System.out.println("Bir Sayı Giriniz :");
+        int n = klavye.nextInt();
+
+         for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+         for (int i = n; i >= 0; --i) {
+            for (int j = (n - i); j >= 1; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i + 1); k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
