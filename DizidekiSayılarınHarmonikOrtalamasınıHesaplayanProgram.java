32a79a4dfb4f50acd678a0dd95e7e84c97555897
denizOgut <46030809+denizOgut@users.noreply.github.com>
2021-09-25 14:13:47 +0300

    Add files via upload

---
+++ DizidekiSayılarınHarmonikOrtalamasınıHesaplayanProgram.java
@@ -0,0 +1,23 @@
+package dizidekisayılarınharmonikortalamasınıhesaplayanprogram;
+
+public class DizidekiSayılarınHarmonikOrtalamasınıHesaplayanProgram {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        System.out.println(sum / numbers.length);
+        
+        //*******************************************************//
+        
+        sum = 0;
+          for (int i = 0; i < numbers.length; i++) {
+            sum += (1.0 / numbers[i]);
+        }
+        System.out.println("sum: " + (numbers.length / sum));
+    }
+
+}
